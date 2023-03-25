22869ca3d1ea7cb598bfd751d268b17d16149d01
ItsVladOoO <vladscraba2003@gmail.com>
2023-03-25 16:39:54 -0400

    Singleton Update to Account Manager

--- BookStoreApplication/src/bookstoreapplication/AccountManager.java
+++ BookStoreApplication/src/bookstoreapplication/AccountManager.java
@@ -15,9 +15,18 @@ import java.util.ArrayList;
  */
 public class AccountManager {
     private ArrayList<UserEntity> userList;
+    private static AccountManager instance;
     
-    public AccountManager(){
+    private AccountManager(){
         userList = new ArrayList<UserEntity>();
+    }
+    
+    public static AccountManager getInstance(){
+        if(instance == null){
+            instance = new AccountManager();
+        }
+        
+        return instance;
     }
     
     public ArrayList getUserList(){
