b76737285e49859eea2e80a3a117c3776609aa80
ItsVladOoO <vladscraba2003@gmail.com>
2023-03-25 16:26:25 -0400

    AccountManager Class Edit

--- BookStoreApplication/src/bookstoreapplication/AccountManager.java
+++ BookStoreApplication/src/bookstoreapplication/AccountManager.java
@@ -5,10 +5,30 @@
  */
 package bookstoreapplication;
 
+import bookstoreapplication.DataStructures.CustomerData;
+import bookstoreapplication.DataStructures.UserEntity;
+import java.util.ArrayList;
+
 /**
  *
  * @author Victor
  */
 public class AccountManager {
+    private ArrayList<UserEntity> userList;
+    
+    public AccountManager(){
+        userList = new ArrayList<UserEntity>();
+    }
+    
+    public ArrayList getUserList(){
+        return userList;
+    }
+    
+    public void addCustomer(String user, String pass){
+        CustomerData a = new CustomerData(user, pass, 0);
+        userList.add(a);
+    }
+    
+    
     
 }
