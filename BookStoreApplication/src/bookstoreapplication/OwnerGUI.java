d1451a8f9822555df46d8271c49fd1169e78efbe
Victor2266 <46388269+Victor2266@users.noreply.github.com>
2023-03-25 16:04:55 -0400

    added accessUI to GUI classes

--- BookStoreApplication/src/bookstoreapplication/OwnerGUI.java
+++ BookStoreApplication/src/bookstoreapplication/OwnerGUI.java
@@ -5,10 +5,53 @@
  */
 package bookstoreapplication;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
 /**
  *
  * @author deeps
  */
-public class OwnerGUI {
-    
+public class OwnerGUI extends ApplicationGUI{
+        public void accessUI(Stage primaryStage) {
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+        Button btn2 = new Button();
+        btn2.setText("Say 'test'");
+        btn2.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("setset!");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        VBox vbox = new VBox();
+  
+        vbox.getChildren().add(btn);
+        vbox.setAlignment(Pos.CENTER);
+        vbox.setSpacing(20);
+        
+        vbox.getChildren().add(btn2);
+        root.getChildren().add(vbox);
+        Scene scene = new Scene(root, 560, 480);
+        
+        primaryStage.setTitle("Book Store Application");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 }
