b1323099659dd6d7f05fbd0d68664874250e496f
deep-patel21 <103757105+deep-patel21@users.noreply.github.com>
2023-03-25 16:00:07 -0400

    Created customerGUI and ownerGUI

--- BookStoreApplication/src/bookstoreapplication/ApplicationGUI.java
+++ BookStoreApplication/src/bookstoreapplication/ApplicationGUI.java
@@ -1,14 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//Deep Patel
+
 package bookstoreapplication;
 
-/**
- *
- * @author deeps
- */
-public class ApplicationGUI {
-    
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class ApplicationGUI extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Bookstore Application - Group 3 COE528");
+    }
 }
