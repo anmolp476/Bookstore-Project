a64f9650c35b271b1e547471812d5dd94e864514
Yanny Patel <47578696+Yanny24211@users.noreply.github.com>
2023-03-25 18:31:48 -0400

    Created FileGateway Class
    created class and added blank methods

---
+++ BookStoreApplication/src/bookstoreapplication/FileGateway.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bookstoreapplication;
+
+/**
+ *
+ * @author Yanny
+ */
+public class FileGateway {
+    public UserData readUserFile(){
+    
+    }
+    
+    public BookData readBookFile(){
+        
+    }
+    
+    public boolean writeUserFile(List<UserData> userDataList){
+        
+    }
+    
+    public boolean writeBookFile(List<BookData> bookDataList){
+        
+    }
+    
+}
