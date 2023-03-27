b004b58f35f6b80fecabbcef9024219877bfae8a
Victor2266 <46388269+Victor2266@users.noreply.github.com>
2023-03-26 22:20:25 -0400

    formatting

--- BookStoreApplication/src/bookstoreapplication/GUIs/OwnerGUI.java
+++ BookStoreApplication/src/bookstoreapplication/GUIs/OwnerGUI.java
@@ -50,76 +50,75 @@ public class OwnerGUI extends ApplicationGUI{
         this.LM = LM;
     }
     
-    
-    
-        public void accessUI(Stage primaryStage) {
-            
-            Button btn1 = new Button();
-            String buttonStyle = "-fx-font-family: Helvetica; -fx-background-color: #fff; -fx-font-size: 16pt;";
-            btn1.setText("Books");
-            btn1.setOnAction(new EventHandler<ActionEvent>() {
-                @Override
-                public void handle(ActionEvent event) {
-                    System.out.println("Books test");
-                    primaryStage.setScene(Owner_Books_Scene);
-                }
-            });
-            Button btn2 = new Button();
-            btn2.setText("Customers");
-            btn2.setOnAction(new EventHandler<ActionEvent>() {
-                @Override
-                public void handle(ActionEvent event) {
-                    System.out.println("Customer test");
-                    primaryStage.setScene(Owner_Customers_Scene);
-                }
-            });
-            Button btn3 = new Button();
-            btn3.setText("Logout");
-            btn3.setOnAction(new EventHandler<ActionEvent>() {
-                @Override
-                public void handle(ActionEvent event) {
-                    System.out.println("Logout Test");
-                    logoutSequence(primaryStage);
-                }
-            });
-            
-            SetupChildScenes(btn1, btn2, primaryStage);
-            
-            btn1.setStyle(buttonStyle);
-            btn2.setStyle(buttonStyle);
-            btn3.setStyle(buttonStyle);
-            
-            /*Image logo = new Image("File:Media/BookstoreApplicationLogo.png");
-            ImageView imageViewer = new ImageView(logo);
-            imageViewer.setFitWidth(75);
-            imageViewer.setFitWidth(75); */
-            
-            Text mainTitle = new Text("Owner Terminal");
-            mainTitle.setFont(Font.font("Helvetica", FontWeight.BOLD, 35));
- 
-    
-            StackPane root = new StackPane();
-            root.setStyle("-fx-background-color: #e6e0da;");
-            
-            /*VBox imageBox = new VBox();
-            imageBox.setAlignment(Pos.CENTER_LEFT);
-            imageBox.setSpacing(15);
-            imageBox.getChildren().add(imageViewer); */
-            
-            VBox vbox = new VBox();
-            vbox.getChildren().add(mainTitle);
-            vbox.getChildren().add(btn1);
-            vbox.setAlignment(Pos.CENTER);
-            vbox.setSpacing(20);
-            vbox.getChildren().add(btn2);
-            root.getChildren().add(vbox);
-            vbox.getChildren().add(btn3);
-            
-            Scene scene = new Scene(root, LoginGUI.defaultWidth, LoginGUI.defaultHeight);
 
-            primaryStage.setTitle("Book Store Application");
-            primaryStage.setScene(scene);
-            primaryStage.show();
+    public void accessUI(Stage primaryStage) {
+
+        Button btn1 = new Button();
+        String buttonStyle = "-fx-font-family: Helvetica; -fx-background-color: #fff; -fx-font-size: 16pt;";
+        btn1.setText("Books");
+        btn1.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Books test");
+                primaryStage.setScene(Owner_Books_Scene);
+            }
+        });
+        Button btn2 = new Button();
+        btn2.setText("Customers");
+        btn2.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Customer test");
+                primaryStage.setScene(Owner_Customers_Scene);
+            }
+        });
+        Button btn3 = new Button();
+        btn3.setText("Logout");
+        btn3.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Logout Test");
+                logoutSequence(primaryStage);
+            }
+        });
+
+        SetupChildScenes(btn1, btn2, primaryStage);
+
+        btn1.setStyle(buttonStyle);
+        btn2.setStyle(buttonStyle);
+        btn3.setStyle(buttonStyle);
+
+        /*Image logo = new Image("File:Media/BookstoreApplicationLogo.png");
+        ImageView imageViewer = new ImageView(logo);
+        imageViewer.setFitWidth(75);
+        imageViewer.setFitWidth(75); */
+
+        Text mainTitle = new Text("Owner Terminal");
+        mainTitle.setFont(Font.font("Helvetica", FontWeight.BOLD, 35));
+
+
+        StackPane root = new StackPane();
+        root.setStyle("-fx-background-color: #e6e0da;");
+
+        /*VBox imageBox = new VBox();
+        imageBox.setAlignment(Pos.CENTER_LEFT);
+        imageBox.setSpacing(15);
+        imageBox.getChildren().add(imageViewer); */
+
+        VBox vbox = new VBox();
+        vbox.getChildren().add(mainTitle);
+        vbox.getChildren().add(btn1);
+        vbox.setAlignment(Pos.CENTER);
+        vbox.setSpacing(20);
+        vbox.getChildren().add(btn2);
+        root.getChildren().add(vbox);
+        vbox.getChildren().add(btn3);
+
+        Scene scene = new Scene(root, LoginGUI.defaultWidth, LoginGUI.defaultHeight);
+
+        primaryStage.setTitle("Book Store Application");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
         
     private void SetupChildScenes(Button BooksButton, Button CustomersButton, Stage primaryStage){
