f0f84e3c691c5c27394b9cb01fcf0727fc038528
deep-patel21 <103757105+deep-patel21@users.noreply.github.com>
2023-03-26 21:04:24 -0400

    Added book selection screen

--- BookStoreApplication/src/bookstoreapplication/GUIs/CustomerGUI.java
+++ BookStoreApplication/src/bookstoreapplication/GUIs/CustomerGUI.java
@@ -30,6 +30,11 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.control.TableColumn;
+import java.util.ArrayList;
+import java.util.List;
+import javafx.collections.FXCollections;
 
 /**
  *
@@ -37,12 +42,43 @@ import javafx.stage.Stage;
  */
 public class CustomerGUI extends ApplicationGUI{
     public void accessUI(Stage primaryStage) {
+        
+        TableView<Book> table = new TableView<>();
+        table.setEditable(true);
+        
+        List<Book> books = new ArrayList<>();
+        //books.add(new Book("Temp", 99.99));
+        //books.add(new Book("Temp", 99.99));
+        table.setItems(FXCollections.observableArrayList(books));
+        
         Label topParagraph = new Label("Welcome Jane. You have X points. Your status is Y.");
         BorderPane.setAlignment(topParagraph, Pos.CENTER);
-     
+    
+        
+        TableColumn<Book, String> col1 = new TableColumn<>("Name of Book");
+        TableColumn<Book, Double> col2 = new TableColumn<>("Price of Book");
+        TableColumn<Book, Boolean> col3 = new TableColumn<>("Selection Box");
+        
+        double tableWidth = table.getPrefWidth();
+       
+        col1.prefWidthProperty().bind(table.widthProperty().multiply(0.4));
+        col2.prefWidthProperty().bind(table.widthProperty().multiply(0.3));
+        col3.prefWidthProperty().bind(table.widthProperty().multiply(0.4));
+        
+        table.getColumns().addAll(col1, col2, col3);
+      
+        Button buyBtn = new Button("Buy");
+        Button redeemBtn = new Button("Redeem Points to Buy");
+        Button logoutBtn = new Button("Logout");
+        
+        FlowPane buttons = new FlowPane(10, 10, buyBtn, redeemBtn, logoutBtn);
+        buttons.setAlignment(Pos.CENTER);
+        
         BorderPane root = new BorderPane();
         root.setTop(topParagraph);
-        Scene scene = new Scene(root, 560, 480);
+        root.setCenter(table);
+        root.setBottom(buttons);
+        Scene scene = new Scene(root, 600, 400);
 
         primaryStage.setTitle("Book Store Application");
         primaryStage.setScene(scene);
