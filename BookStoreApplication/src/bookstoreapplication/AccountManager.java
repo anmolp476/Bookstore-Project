4df79e5dd9626766797a4b9999e634644bb3d82a
Victor2266 <46388269+Victor2266@users.noreply.github.com>
2023-03-28 21:58:52 -0400

    Update AccountManager.java

--- BookStoreApplication/src/bookstoreapplication/AccountManager.java
+++ BookStoreApplication/src/bookstoreapplication/AccountManager.java
@@ -15,86 +15,89 @@ import java.util.ArrayList;
  * @author Victor
  */
 public class AccountManager {
+
     private ArrayList<UserEntity> userList;
     private static AccountManager instance;
-    
-    private AccountManager(){
+
+    private AccountManager() {
         userList = new ArrayList<UserEntity>();
-        if(!checkUserExists("admin")){
-            addOwner("admin", "admin");
-        }
+
     }
-    
-    public static AccountManager getInstance(){
-        if(instance == null){
+
+    public static AccountManager getInstance() {
+        if (instance == null) {
             instance = new AccountManager();
         }
         return instance;
     }
-    
-    public void loadUserList(ArrayList<UserEntity> userList){
-        if(userList == null) return;
-        
-        for(UserEntity user : userList){
+
+    public void loadUserList(ArrayList<UserEntity> userList) {
+        if (userList == null) {
+            return;
+        }
+
+        for (UserEntity user : userList) {
             this.userList.add(user);
             user.UnSelectUser();
         }
+        if (!checkUserExists("admin")) {
+            addOwner("admin", "admin");
+        }
     }
-    
-    public ArrayList<UserEntity> getUserList(){
+
+    public ArrayList<UserEntity> getUserList() {
         return userList;
     }
-    
-    public UserEntity getUser(String user){
-        for (UserEntity i : userList){
-            if (i.getUsername().equals(user)){
+
+    public UserEntity getUser(String user) {
+        for (UserEntity i : userList) {
+            if (i.getUsername().equals(user)) {
                 return i;
-            }   
+            }
         }
-        return null;        
+        return null;
     }
-    
-    public void addCustomer(String user, String pass){    
-        if (!checkUserExists(user)){
+
+    public void addCustomer(String user, String pass) {
+        if (!checkUserExists(user)) {
             CustomerData a = new CustomerData(user, pass, 0);
             userList.add(a);
         }
     }
-        
-    public void addOwner(String user, String pass){    
-        if (!checkUserExists(user)){
+
+    public void addOwner(String user, String pass) {
+        if (!checkUserExists(user)) {
             OwnerData a = new OwnerData(user, pass);
             userList.add(a);
         }
     }
-    
-    public boolean checkUserExists(String user){  
-        for (UserEntity i : userList){
-            if (i.getUsername().equals(user)){
+
+    public boolean checkUserExists(String user) {
+        for (UserEntity i : userList) {
+            if (i.getUsername().equals(user)) {
                 return true;
-            }   
+            }
         }
         return false;
     }
-    
-    public CustomerData getCustomer(String user){
-        for (UserEntity i : userList){
-            if (i.getUsername().equals(user)){
-                return (CustomerData)i;
-            }   
+
+    public CustomerData getCustomer(String user) {
+        for (UserEntity i : userList) {
+            if (i.getUsername().equals(user)) {
+                return (CustomerData) i;
+            }
         }
         return null;
     }
-    
-    public boolean removeCustomer(String user){
-        for (UserEntity i : userList){
-            if (i.getUsername().equals(user)){
+
+    public boolean removeCustomer(String user) {
+        for (UserEntity i : userList) {
+            if (i.getUsername().equals(user)) {
                 userList.remove(i);
                 return true;
-            }   
+            }
         }
         return false;
     }
-    
-    
+
 }
